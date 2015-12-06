f52c5f70a56d731cd816cab57b853de5503ecb34
Tom Newman <tnewman1@ltu.edu>
2015-12-06 16:23:43 -0500

    Comment Binary Thresholds

--- VCCChallenge/Model/BinaryThresholds.cs
+++ VCCChallenge/Model/BinaryThresholds.cs
@@ -2,6 +2,9 @@
 
 namespace VCCChallenge
 {
+    /// <summary>
+    /// Represents Binary Thresholds used for contour extraction.
+    /// </summary>
     class BinaryThresholds
     {
         public BinaryThresholds()
@@ -14,11 +17,34 @@ namespace VCCChallenge
             this.ValMax = 0;
         }
 
+        /// <summary>
+        /// Minimum Hue
+        /// </summary>
         public int HueMin { get; set; }
+
+        /// <summary>
+        /// Maximum Hue
+        /// </summary>
         public int HueMax { get; set; }
+
+        /// <summary>
+        /// Minimum Saturation
+        /// </summary>
         public int SatMin { get; set; }
+
+        /// <summary>
+        /// Maximum Saturation
+        /// </summary>
         public int SatMax { get; set; }
+
+        /// <summary>
+        /// Minimum Value
+        /// </summary>
         public int ValMin { get; set; }
+
+        /// <summary>
+        /// Maximum Value
+        /// </summary>
         public int ValMax { get; set; }
     }
 }
